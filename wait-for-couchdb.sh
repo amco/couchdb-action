6eba85d7640ee239d2645ef140702dbd195637a9
javierg <tijuanate@gmail.com>
2025-12-01 08:14:42 -0800

    get container status

--- wait-for-couchdb.sh
+++ wait-for-couchdb.sh
@@ -4,7 +4,7 @@ echo "Waiting for CouchDB..."
 retries=0
 max_retries=20
 
-until curl --output /dev/null --silent --head --fail http://localhost:5984/; do
+until curl --output /dev/null --silent --head --fail http://127.0.0.1:5984/; do
   retries=$((retries+1))
   if [ $retries -ge $max_retries ]; then
     echo "CouchDB did not become available after $max_retries attempts. Exiting."
